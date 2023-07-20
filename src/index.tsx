dc4751bdce5ffcef621901b31779e59180580c2a
Ahmad Anwar <7552088+AhmedAnwarHafez@users.noreply.github.com>
2023-07-21 07:27:17 +1200

    refactor and create a header component

--- src/index.tsx
+++ src/index.tsx
@@ -1,38 +1,25 @@
-import { Elysia } from "elysia";
-import { html } from "@elysiajs/html";
-import * as elements from "typed-html";
-
-const BaseHtml = ({ children }: elements.Children) => `
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>THE BETH STACK</title>
-  <script src="https://unpkg.com/htmx.org@1.9.3"></script>
-  <script src="https://unpkg.com/hyperscript.org@0.9.9"></script>
-  <script src="https://cdn.tailwindcss.com"></script>
-  <link href="/styles.css" rel="stylesheet">
-</head>
-
-${children}
-`;
+import { Elysia } from 'elysia'
+import { html } from '@elysiajs/html'
+import * as elements from 'typed-html'
+import { Layout } from './layout'
+import { Header } from './components/Header'
 
 const app = new Elysia()
   .use(html())
-  .get("/", ({ html }) =>
+  .get('/', ({ html }) =>
     html(
-      <BaseHtml>
-        <body class="bg-slate-300">here is the body</body>
-      </BaseHtml>,
-    ),
+      <Layout>
+        <body class="bg-slate-300">
+          <Header />
+        </body>
+      </Layout>
+    )
   )
   // no need to have a public folder
   // this route will serve the css as a static file
-  .get("/styles.css", () => Bun.file("./src/input.css"))
-  .listen(3000);
+  .get('/styles.css', () => Bun.file('./src/input.css'))
+  .listen(3000)
 
 console.log(
-  `🦊 Elysia is running at on port http://${app.server.hostname}:${app.server.port}...`,
-);
+  `🦊 Elysia is running at on port http://${app.server.hostname}:${app.server.port}...`
+)
