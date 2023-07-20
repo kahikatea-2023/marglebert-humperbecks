01275fb795d050233bb042d1dec61e220d1e5184
Ahmad Anwar <7552088+AhmedAnwarHafez@users.noreply.github.com>
2023-07-21 07:33:06 +1200

    add styling to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,11 +2,17 @@ import * as elements from 'typed-html'
 
 export function Header() {
   return (
-    <header class="bg-white shadow">
-      <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-        <h1 class="text-3xl font-bold leading-tight text-gray-900">
-          Dashboard
-        </h1>
+    <header class="bg-red-500">
+      <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 flex items-center gap-4">
+        <h1 class="text-7xl font-bold leading-tight text-gray-900">Marbecks</h1>
+        <div>
+          <h1 class="text-3xl font-bold leading-tight text-gray-900 tracking-tighter">
+            RECORD SHOP
+          </h1>
+          <h2 class="text-3xl font-bold  text-gray-900 tracking-widest">
+            EST. 1934
+          </h2>
+        </div>
       </div>
     </header>
   )
