edbb31a1f002f47c182bf5dedb9560dd42b5ec3e
Ahmad Anwar <7552088+AhmedAnwarHafez@users.noreply.github.com>
2023-07-21 05:50:11 +1200

    update schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,10 +1,9 @@
 import { InferModel } from "drizzle-orm";
 import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
-export const todos = sqliteTable("todos", {
+export const todos = sqliteTable("albums", {
   id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
-  content: text("content").notNull(),
-  completed: integer("completed", { mode: "boolean" }).notNull().default(false),
+  content: text("name").notNull(),
 });
 
 export type Todo = InferModel<typeof todos>;
