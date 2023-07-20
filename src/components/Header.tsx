a40a5e769464ba072f0e7c32bdbefb35eb301bdf
Ahmad Anwar <7552088+AhmedAnwarHafez@users.noreply.github.com>
2023-07-21 07:38:56 +1200

    style header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,8 +2,8 @@ import * as elements from 'typed-html'
 
 export function Header() {
   return (
-    <header class="bg-red-500">
-      <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 flex items-center gap-4">
+    <header class="bg-red-500 flex items-stretch">
+      <div class="flex items-center gap-4">
         <h1 class="text-7xl font-bold leading-tight text-gray-900">Marbecks</h1>
         <div>
           <h1 class="text-3xl font-bold leading-tight text-gray-900 tracking-tighter">
@@ -13,6 +13,11 @@ export function Header() {
             EST. 1934
           </h2>
         </div>
+      </div>
+      <div class="place-self-end">
+        <p class="text-white uppercase">
+          New Zealand's Leading Music Specialist
+        </p>
       </div>
     </header>
   )
