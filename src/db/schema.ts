b9f08e6e5f405d6cdb1fc64643ae33a996f39107
Ahmad Anwar <7552088+AhmedAnwarHafez@users.noreply.github.com>
2023-07-21 06:56:36 +1200

    add todo

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,9 +1,11 @@
 import { InferModel } from "drizzle-orm";
 import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
+// this is the only table we need to define
 export const todos = sqliteTable("albums", {
   id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
   content: text("name").notNull(),
+  // TODO: add release date, artist, price
 });
 
 export type Todo = InferModel<typeof todos>;
