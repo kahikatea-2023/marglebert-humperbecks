a41da34625678a811aca92d34060760a8645e1f9
Ahmad Anwar <7552088+AhmedAnwarHafez@users.noreply.github.com>
2023-07-21 11:33:11 +1200

    surround the search input with a form

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -3,12 +3,17 @@ import * as elements from 'typed-html'
 export function SearchInput() {
   return (
     <div class="flex flex-row justify-end">
-      <input
-        class="border-solid border-2 border-gray-400"
-        type="search"
-        placeholder="Search for an album"
-      />
-      <button class="border-solid border-2 border-gray-400">Search</button>
+      <form action="/search" method="post">
+        <label for="search-input">Search</label>
+        <input
+          id="search-input"
+          class="border-solid border-2 border-gray-400"
+          type="search"
+          name="q"
+          placeholder="Search for an album"
+        />
+        <button class="border-solid border-2 border-gray-400">Search</button>
+      </form>
     </div>
   )
 }
