61d12dfda8227ea6ce06ba18e4fe43457f21b246
Ahmad Anwar <7552088+AhmedAnwarHafez@users.noreply.github.com>
2023-07-22 13:59:24 +1200

    add fontawesome script tag

--- src/layout.tsx
+++ src/layout.tsx
@@ -10,6 +10,7 @@ export const Layout = ({ children }: elements.Children) => `
   <title>THE BETH STACK</title>
   <script src="https://unpkg.com/htmx.org@1.9.3"></script>
   <script src="https://unpkg.com/hyperscript.org@0.9.9"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/js/all.min.js" integrity="sha512-fD9DI5bZwQxOi7MhYWnnNPlvXdp/2Pj3XSTRrFs5FQa4mizyGLnJcN6tuvUS6LbmgN1ut+XGSABKvjN0H6Aoow==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
   <link href="/styles.css" rel="stylesheet">
 </head>
 
